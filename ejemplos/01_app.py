d0eb5e0cc03f1a0b13de372c87872b1b020c476c
Juan Ortega <juan.fer.ortega10@gmail.com>
2024-08-31 00:37:14 +0000

    UPDATE README.md

---
+++ ejemplos/01_app.py
@@ -0,0 +1,23 @@
+"""
+curl -X POST http://localhost:11434/api/generate -d '{
+  "model": "tinyllama",
+  "prompt": "Why is the sky blue?",
+  "stream": false
+}' 
+"""
+
+import requests
+import json
+
+url = 'http://localhost:11434/api/generate'
+data = {
+    "model": "tinyllama",
+    "prompt": "Why is the sky blue?",
+    "stream": False
+    }
+
+response = requests.post(url, json = data)
+
+response = json.loads(response.text)
+
+print(response["response"])
